38ea1711270bb6cf026500f17373c86cfa8d1be9
kkr <mosbisu@gmail.com>
2020-07-08 16:39:16 +0900

    first commit

--- app/src/main/java/com/kkr95/howweather/LoginActivity.java
+++ app/src/main/java/com/kkr95/howweather/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); 
 
         String keyHash= getKeyHash(this);
         Log.i("TAG", keyHash);
